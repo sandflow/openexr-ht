5a52ab47753dcb1ecb32b0eb2aa90a28f4201da2
Pierre-Anthony Lemieux <pal@palemieux.com>
2025-01-27 08:43:13 -0800

    Fix Windows

--- share/ci/scripts/install_openjph.sh
+++ share/ci/scripts/install_openjph.sh
@@ -20,7 +20,7 @@ cd OpenJPH
 cd build
 cmake -DOJPH_ENABLE_TIFF_SUPPORT=OFF -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
-$SUDO cmake --install
+$SUDO cmake --install .
 
 cd ../..
 rm -rf OpenJPH
