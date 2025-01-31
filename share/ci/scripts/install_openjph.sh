8e3291dd20aa535f4f9b8c7111e86b11bef0762e
Pierre-Anthony Lemieux <pal@palemieux.com>
2025-01-31 12:53:57 -0800

    Fix CI

--- share/ci/scripts/install_openjph.sh
+++ share/ci/scripts/install_openjph.sh
@@ -18,11 +18,11 @@ cd OpenJPH
 # git checkout ${TAG}
 
 cd build
+cmake -DOJPH_ENABLE_TIFF_SUPPORT=OFF -DCMAKE_BUILD_TYPE=Release .. 
 $SUDO cmake --build . \
       --target install \
       --config Release \
-      --parallel 2 \
-      -- -DOJPH_ENABLE_TIFF_SUPPORT=OFF
+      --parallel 2
 
 cd ../..
 rm -rf OpenJPH
